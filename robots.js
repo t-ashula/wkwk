d6c81c89ddd026748c31a125abee944c1871df2b
t-ashula <t.ashula+github@gmail.com>
2023-01-24 16:31:36 +0900

    :recycle: refactor fetch/makeurl

--- robots.js
+++ robots.js
@@ -2,30 +2,54 @@ import { getCurrentTab } from "./chrome.js";
 import { E, T } from "./dom.js";
 
 /**
- * 
+ *
+ * @param {string} url
+ * @returns {string} '/robots.txt' url
+ */
+function makeRobotsUrl(url) {
+    const robotsUrl = new URL(url);
+    robotsUrl.pathname = "/robots.txt";
+    robotsUrl.search = '';
+    robotsUrl.hash = '';
+    return robotsUrl.href;
+}
+
+/**
+ *
+ * @param {string} url
+ * @returns {Object} { text: "contents of robots.txt", status: http.status, error: "if error occured." }
+ */
+async function fetchRobotsTxt(url) {
+    try {
+        const res = await fetch(url, { headers: { 'Accept': 'text/plain' } });
+        if (res) {
+            const text = await res.text();
+            return { status: res.status, text: text };
+        } else {
+            return { error: `fetch failed. [url=${url}, error="no response"]` };
+        }
+    } catch (err) {
+        return { error: `exception raised. [url=${url}, error="${err}"]` };
+    }
+}
+
+/**
+ *
  */
 export default async function robots() {
     const tab = await getCurrentTab();
-    const div = E("div", { id: "robots-result" }, E("h2", {}, T("robots.txt")), E("p", {}, T(`url: ${tab.url}`)));
-    try {
-        const robotsUrl = new URL(tab.url);
-        robotsUrl.pathname = "/robots.txt";
-        const res = await fetch(robotsUrl.href);
-        if (res) {
-            if (res.status === 200) {
-
-                const text = await res.text();
-                div.appendChild(E("p", {}, E("a", { href: robotsUrl.href }, T("found."))));
-                div.appendChild(E("textarea", {}, T(text)));
-
-            } else {
-                div.appendChild(E("p", {}, T(`not found. ${res.status}`)));
-            }
+    const robotsUrl = makeRobotsUrl(tab.url);
+    const div = E("div", { id: "robots-result" }, E("h2", {}, T("robots.txt")), E("p", {}, T(`url: ${robotsUrl}`)));
+    const result = await fetchRobotsTxt(robotsUrl);
+    if (result.status) {
+        if (result.status === 200) {
+            div.appendChild(E("p", {}, E("a", { href: robotsUrl, target: "_blank" }, T("found."))));
+            div.appendChild(E("textarea", {}, T(result.text)));
         } else {
-            div.appendChild(T("something error"));
+            div.appendChild(E("p", {}, T(`not found. ${result.status}, ${result.text}`)));
         }
-    } catch (e) {
-        div.appendChild(T(`something error :${e}, ${JSON.stringify(tab)}`))
+    } else {
+        div.appendChild(T(`something error :${result.error}`))
     }
     return div;
 }
